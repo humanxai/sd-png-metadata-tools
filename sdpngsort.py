ce287df84f080139e313a21e995cb2a7fefa296d
humanxai <122137765+humanxai@users.noreply.github.com>
2023-01-07 13:40:47 +0200

    Update sdpngsort.py
    Ignores files that have a duplicate counterpart.
    Reports ignored files.

--- sdpngsort.py
+++ sdpngsort.py
@@ -1,22 +1,24 @@
 import os
 import re
-import sys
 from collections import defaultdict
 from tqdm import tqdm
 from PIL import Image
+import sys
 
 # Maximum number of pixels allowed in an image
 MAX_PIXELS = 128 * 1000000
 
-if len(sys.argv) < 2:
-  input_folder = os.getcwd()
-else:
+# Check if an input folder was provided as a command-line argument
+if len(sys.argv) > 1:
   input_folder = sys.argv[1]
+else:
+  input_folder = os.getcwd()
 
 # Modify this regular expression to match the model name up until the next ','
 model_re = re.compile(r'Model: (.*?),')
 model_hash_re = re.compile(r'Model hash: (\w+)')
 metadata = []
+ignored_files = 0
 for filename in tqdm(os.listdir(input_folder)):
   if filename.endswith('.png'):
     # Open the image file with PIL.Image.File
@@ -25,6 +27,7 @@ for filename in tqdm(os.listdir(input_folder)):
       width, height = im.size
       if width * height > MAX_PIXELS:
         # Skip images that are too large
+        ignored_files += 1
         continue
 
       metadata_str = str(im.info)
@@ -36,6 +39,7 @@ for filename in tqdm(os.listdir(input_folder)):
         if model_hash_match:
           model = model_hash_match.group(1)
         else:
+          ignored_files += 1
           continue
       metadata.append((filename, model))
 
@@ -43,19 +47,29 @@ files_by_model = defaultdict(list)
 for filename, model in metadata:
   files_by_model[model].append(filename)
 
+moved_files = 0
+ignored_files_model = 0
 for model, filenames in tqdm(files_by_model.items()):
   model_folder = os.path.join(input_folder, model)
 
-# Create the model folder if it does not exist
+  # Create the model folder if it does not exist
   if not os.path.exists(model_folder):
     os.makedirs(model_folder)
 
-# Move the image and text files to the model folder
+  # Move the image and text files to the model folder
   for filename in filenames:
     src_path = os.path.join(input_folder, filename)
     dst_path = os.path.join(model_folder, filename)
-    os.rename(src_path, dst_path)
+    try:
+      os.rename(src_path, dst_path)
+      moved_files += 1
+    except FileExistsError:
+      ignored_files_model += 1
+      continue
     txt_src_path = src_path[:-3] + 'txt'
     txt_dst_path = dst_path[:-3] + 'txt'
     if os.path.exists(txt_src_path):
       os.rename(txt_src_path, txt_dst_path)
+
+print(f'Moved {moved_files} files')
+print(f'Ignored {ignored_files_model} files')
